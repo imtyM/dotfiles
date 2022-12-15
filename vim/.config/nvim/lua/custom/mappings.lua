578f0c645bab1899ad147ea8c1e6575ff94f6412
imtyM <m.imtiazmukadam@gmail.com>
2022-12-15 20:14:45 +0200

    update: nvchad

--- vim/.config/nvim/lua/custom/mappings.lua
+++ vim/.config/nvim/lua/custom/mappings.lua
@@ -2,6 +2,9 @@ local M = {}
 
 M.disabled = {
   n = {
+    -- Overide
+    ["<S-TAB>"] = {""},
+    ["<TAB>"] = {""},
   },
 
   i = {
@@ -15,17 +18,27 @@ M.misc = {
     ["<leader>gg"] = { ":tabnew <CR>:G<CR>:winc k<CR>:q<CR>:NvimTreeClose<CR>", "Git buffer in new window"},
 
     ["<C-p>"] = {":Telescope git_files follow=true hidden=true <CR>", "Search project files"},
+    ["<C-w>"] = {":TrailerTrim | wa<CR>", "Write all buffers"},
+    ["<S-TAB>"] = {":tabp<CR>", "Previous tab"},
+    ["<TAB>"] = {":tabn<CR>", "Next tab"},
 
   },
 
   i = {
     -- Exit insert mode
     ["<C-j>"] = {"<ESC>", "Escape"},
+    ["<C-w>"] = {"<ESC>:TrailerTrim | wa <CR>", "Write all buffers"},
   },
 
   v = {
     ["<C-j>"] = {"<ESC>", "Escape"},
   }
 }
+
+--Easymotion
+vim.cmd [[
+map  / <Plug>(easymotion-sn)
+omap / <Plug>(easymotion-tn)
+]]
 
 return M
