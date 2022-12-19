2a352045cf15a585cb717361ee9fb21201145d68
imtyM <m.imtiazmukadam@gmail.com>
2022-12-19 22:44:06 +0200

    Fresh start with kickstart

--- vim/.config/nvim/lua/custom/sets.lua
+++
@@ -1,9 +0,0 @@
-local opt = vim.opt
-
-opt.cursorline = false
-
-vim.cmd [[
-set nohlsearch
-let g:trailertrash_blacklist = ['__Calendar', '^http:', 'mason.nvim']
-hi UnwantedTrailerTrash guibg= ctermbg=
-]]
