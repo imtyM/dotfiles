4ba01cfbca9992175e7462d97fc8807c3dc7f832
imtyM <m.imtiazmukadam@gmail.com>
2024-09-15 15:45:53 +0200

    update: nvim

--- nvim/.config/lazy/lua/config/keymaps.lua
+++ nvim/.config/lazy/lua/config/keymaps.lua
@@ -29,5 +29,3 @@ vim.keymap.set("n", "gv", "<cmd>vs<CR> gd", { desc = "Go to def in a new split" 
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
